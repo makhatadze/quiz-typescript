ffe82d8c3d8076dd92da182e1413ad94a71455e1
makhatadze <vitomaxatadze@gmail.com>
2021-04-21 10:48:25 +0400

    Show content into QuestionCard.

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,17 +1,39 @@
-import React from "react";
+import React, {FC} from "react";
 
 type questionCardProps = {
     question: string;
-    answer: string[];
+    answers: string[];
     callback: any;
-    userAnswer: string;
+    userAnswer: boolean;
     questionNumber: number;
     totalQuestions: number;
 }
 
-const QuestionCard = () => (
+const QuestionCard: FC<questionCardProps> = (
+    {
+        question,
+        answers,
+        callback,
+        userAnswer,
+        questionNumber,
+        totalQuestions
+    }) => (
     <div>
-
+        <p className={'number'}>
+            Question: {questionNumber} / {totalQuestions}
+        </p>
+        <p dangerouslySetInnerHTML={{__html: question}}/>
+        <div>
+            {answers.map(answer => (
+                <div>
+                    <button
+                        disabled={userAnswer}
+                        onClick={callback}>
+                        <span dangerouslySetInnerHTML={{__html: answer}}/>
+                    </button>
+                </div>
+            ))}
+        </div>
     </div>
 )
 
