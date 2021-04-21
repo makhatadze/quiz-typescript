916d82005c8a7bb6c403c89ec4dd90b834dd85e2
makhatadze <vitomaxatadze@gmail.com>
2021-04-21 11:26:18 +0400

    fetchQuizQuestion return with answers array.

--- src/API.ts
+++ src/API.ts
@@ -1,8 +1,10 @@
+import {shuffleArray} from "./utils";
+
 export type Question = {
     category: string;
     correct_answer: string;
     difficulty: string;
-    incorrect_answer: string[];
+    incorrect_answers: string[];
     question: string;
     type: string;
 }
@@ -20,6 +22,10 @@ export enum Difficulty {
 export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty) => {
     const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
     const data = await (await fetch(endpoint)).json();
-    console.log(amount)
-    console.log(data)
+    return data.results.map((question: Question) => (
+        {
+            ...question,
+            answer: shuffleArray([...question.incorrect_answers, question.correct_answer])
+        }
+    ))
 }
