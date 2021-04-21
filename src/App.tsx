711b8500ea8ecdbd2a3b2465ac88a65043f2428c
makhatadze <vitomaxatadze@gmail.com>
2021-04-21 11:52:18 +0400

    Add nextQuestion logic.

--- src/App.tsx
+++ src/App.tsx
@@ -57,7 +57,14 @@ const App = () => {
     }
 
     const nextQuestion = () => {
+        // Move on to the next question if not the last question
+        const nextQuestion = number + 1;
 
+        if (nextQuestion === TOTAL_QUESTIONS) {
+            setQuizOver(true);
+        } else {
+            setNumber(nextQuestion);
+        }
     }
 
     return (
