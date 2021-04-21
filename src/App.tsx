77246d46f8d7680c6d608ec805c087a0b24b05b7
makhatadze <vitomaxatadze@gmail.com>
2021-04-21 12:12:47 +0400

    Give styles to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, {MouseEvent, useState} from 'react';
 import {Difficulty, fetchQuizQuestions, QuestionState} from "./API";
 import QuestionCard from "./components/QuestionCard";
-
+import {GlobalStyle, Wrapper} from "./App.styles";
 
 const TOTAL_QUESTIONS = 10;
 
@@ -68,37 +68,40 @@ const App = () => {
     }
 
     return (
-        <div className="App">
-            <h1>QUIZ APPLICATION</h1>
-            {quizOver || userAnswers.length === TOTAL_QUESTIONS ? (
-                <button
-                    className={'start'}
-                    onClick={startQuiz}>
-                    Start
-                </button>
-            ) : null}
-            {!quizOver && <p className={'score'}>Score: {score}</p>}
-            {loading && <p>Loading Questions ...</p>}
-            {(!loading && !quizOver) && (
-                <QuestionCard
-                    questionNumber={number + 1}
-                    totalQuestions={TOTAL_QUESTIONS}
-                    question={questions[number].question}
-                    answers={questions[number].answers}
-                    userAnswer={userAnswers ? userAnswers[number] : undefined}
-                    callback={checkAnswer}
-                />
-            )}
-            {!quizOver &&
-            !loading &&
-            userAnswers.length === number + 1 &&
-            number !== TOTAL_QUESTIONS - 1 ? (
-                <button className={'next'} onClick={nextQuestion}>
-                    Next Question
-                </button>
-            ) : null}
+        <>
+            <GlobalStyle/>
+            <Wrapper>
+                <h1>QUIZ APPLICATION</h1>
+                {quizOver || userAnswers.length === TOTAL_QUESTIONS ? (
+                    <button
+                        className={'start'}
+                        onClick={startQuiz}>
+                        Start
+                    </button>
+                ) : null}
+                {!quizOver && <p className={'score'}>Score: {score}</p>}
+                {loading && <p>Loading Questions ...</p>}
+                {(!loading && !quizOver) && (
+                    <QuestionCard
+                        questionNumber={number + 1}
+                        totalQuestions={TOTAL_QUESTIONS}
+                        question={questions[number].question}
+                        answers={questions[number].answers}
+                        userAnswer={userAnswers ? userAnswers[number] : undefined}
+                        callback={checkAnswer}
+                    />
+                )}
+                {!quizOver &&
+                !loading &&
+                userAnswers.length === number + 1 &&
+                number !== TOTAL_QUESTIONS - 1 ? (
+                    <button className={'next'} onClick={nextQuestion}>
+                        Next Question
+                    </button>
+                ) : null}
 
-        </div>
+            </Wrapper>
+        </>
     );
 }
 
