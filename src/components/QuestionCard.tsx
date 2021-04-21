bbc4715d328693fd0b1b8747a73445d345f3915f
makhatadze <vitomaxatadze@gmail.com>
2021-04-21 10:37:42 +0400

    Create questionCardProps.

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,7 +1,19 @@
 import React from "react";
 
+type questionCardProps = {
+    question: string;
+    answer: string[];
+    callback: any;
+    userAnswer: string;
+    questionNumber: number;
+    totalQuestions: number;
+}
 
-const QuestionCard = () => <div>Question Card</div>
+const QuestionCard = () => (
+    <div>
+
+    </div>
+)
 
 
 export default QuestionCard;
