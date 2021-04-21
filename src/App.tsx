061b834e75da4e544e4cdbc7f820a88651c508ec
makhatadze <vitomaxatadze@gmail.com>
2021-04-21 10:35:38 +0400

    Initialize application contents.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, {MouseEvent} from 'react';
+import QuestionCard from "./components/QuestionCard";
 
-const App= () => {
-  return (
-    <div className="App">
+const App = () => {
+    const startQuiz = async () => {
 
-    </div>
-  );
+    }
+
+    const checkAnswer = (e: MouseEvent<HTMLButtonElement>) => {
+
+    }
+
+    const nextQuestion = () => {
+
+    }
+
+    return (
+        <div className="App">
+            <h1>QUIZ APPLICATION</h1>
+            <button
+                className={'start'}
+                onClick={startQuiz}
+            >
+                Start
+            </button>
+            <p className={'score'}>Score:</p>
+            <p>Loading Questions ...</p>
+            <QuestionCard />
+            <button className={'next'} onClick={nextQuestion}>
+                Next Question
+            </button>
+        </div>
+    );
 }
 
 export default App;
