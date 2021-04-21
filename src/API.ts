c9ec2c2ff1735e129ef9e9df9f16a10e8d8118a6
makhatadze <vitomaxatadze@gmail.com>
2021-04-21 11:21:24 +0400

    Create question and questionState types.

--- src/API.ts
+++ src/API.ts
@@ -1,10 +1,23 @@
+export type Question = {
+    category: string;
+    correct_answer: string;
+    difficulty: string;
+    incorrect_answer: string[];
+    question: string;
+    type: string;
+}
+
+export type QuestionState = Question & {
+    answers: string[]
+}
 
 export enum Difficulty {
     EASE = 'easy',
     MEDIUM = 'medium',
     HARD = 'hard'
 }
-export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty)  => {
+
+export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty) => {
     const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
     const data = await (await fetch(endpoint)).json();
     console.log(amount)
